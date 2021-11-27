59147a030a72e4ed35485e468f1e64bb954558d6
Baska <baska.klimekova@gmail.com>
2021-11-28 00:18:50 +0100

    refactoring:

--- src/App.js
+++ src/App.js
@@ -3,19 +3,25 @@ import "./App.css";
 
 import Weather from './components/Weather';
 import Loading from './components/Loading';
+import Forecast from './components/Forecast';
 
 import{cities}  from './utils/index';
+import{forecastDashboard}  from './utils/index';
+
+
 
 const App = () => {
 
   /* API  */
   const MY_API_KEY = process.env.REACT_APP_MY_API_ID;
   const CURRENT_WEATHER_API = 'https://api.openweathermap.org/data/2.5/weather?units=metric&APPID=';
+  const FORECAST_WEATHER_API = 'https://api.openweathermap.org/data/2.5/forecast?units=metric&APPID=';
 
   
   /*useState for weather and for city */
   const [weather, setWeather] =  useState(null);
-  const [city, setCity] =  useState("Prague");
+  const [forecast, setForecast] = useState(null);
+  const [city, setCity] =  useState("Bratislava");
 
 
   /* fetch function to use api and return response with current weather */
@@ -25,15 +31,30 @@ const App = () => {
       .then(json => setWeather(json))
   };
 
+  /* fetch function to use api and return response with 5 days forecast weather */
+  const fetchForecast = () => {
+    fetch( FORECAST_WEATHER_API + MY_API_KEY + '&q=' + city)
+      .then(response => response.json())
+      .then(json => setForecast(forecastDashboard(json.list,8)))
+  };
+
 
   useEffect(() => {
     fetchWeather();
   }, [city]);
 
+  useEffect(() => {
+    fetchForecast();
+  }, [city]);
+
+
   useEffect(fetchWeather, [city]);
+  useEffect(fetchForecast, [weather]);
 
 
-  /* handler for button */
+
+
+  /* handler  */
   const handleOnClickButton = (event) => {
     setCity(event.target.outerText);
   }
@@ -49,9 +70,9 @@ const App = () => {
       <div className="container">
         <h1>My Weather App</h1>
            {<div className="button-group">
+            <button className="button" onClick={handleOnClickButton} >Bratislava</button>
             <button className="button" onClick={handleOnClickButton} >Prague</button>
             <button className="button" onClick={handleOnClickButton} >Berlin</button>
-            <button className="button" onClick={handleOnClickButton} >Amsterdam</button>
           </div> }
        
           <div className="select-wrapper">
@@ -68,7 +89,9 @@ const App = () => {
 
          <div className="weather">
          {weather ? <Weather weather={weather} /> : <Loading />}
-          
+            <div className="weather__forecast" id="predpoved">
+            {forecast ? forecast.map (item => <Forecast data={item} key={item.dt_txt}/>) : <Loading />} 
+            </div>
         </div>
       </div>
     </div>
