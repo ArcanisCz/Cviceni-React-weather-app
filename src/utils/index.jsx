b6b33ac47d4912a681c4df1afc275776fc2d0a19
Baska <baska.klimekova@gmail.com>
2021-11-28 10:01:06 +0100

    refactoring

--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -70,27 +70,4 @@ const cities = [
     "Yakutsk",
   ];  
 
-
-  const weatherData = {
-    main: {
-        temp: "--",
-        humidity: "--"
-        }, 
-    name: "City",
-    sys: {
-        country: "Country",
-        sunrise: 1637479734,
-        sunset: 1637510654
-    },
-    weather: [
-        {
-             description: "--",
-             icon: "01n"
-        }
-    ],
-    wind: {
-        speed: "--"
-    },
-    timezone: 0
-};
-  export {convertedLocalTime,convertedDate, forecastDashboard,weatherData,  cities};
+  export {convertedLocalTime,convertedDate, forecastDashboard, cities};
